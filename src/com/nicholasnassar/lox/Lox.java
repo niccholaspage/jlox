4a15414f45c2bc3f4208a0976d3e09f3d2f81e13
Nicholas Nassar <niccholaspage@gmail.com>
2021-08-12 00:16:06 -0400

    Implement interpreter for expressions (End of Chapter 7)

--- src/com/nicholasnassar/lox/Lox.java
+++ src/com/nicholasnassar/lox/Lox.java
@@ -9,7 +9,9 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+    private static final Interpreter interpreter = new Interpreter();
     private static boolean hadError = false;
+    private static boolean hadRuntimeError = false;
 
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
@@ -29,6 +31,10 @@ public class Lox {
         // Indicate an error in the exit mode.
         if (hadError) {
             System.exit(65);
+        }
+
+        if (hadRuntimeError) {
+            System.exit(70);
         }
     }
 
@@ -58,7 +64,12 @@ public class Lox {
             return;
         }
 
-        System.out.println(new AstPrinter().print(expression));
+        interpreter.interpret(expression);
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() + "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 
     static void error(int line, String message) {
