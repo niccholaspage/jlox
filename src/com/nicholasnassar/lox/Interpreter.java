56921dc586dec5eaac270e236835c857d5a6707c
Nicholas Nassar <niccholaspage@gmail.com>
2021-08-18 21:07:06 -0400

    Add support for logical AND and OR

--- src/com/nicholasnassar/lox/Interpreter.java
+++ src/com/nicholasnassar/lox/Interpreter.java
@@ -129,6 +129,23 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         return expr.value;
     }
 
+    @Override
+    public Object visitLogicalExpr(Expr.Logical expr) {
+        Object left = evaluate(expr.left);
+
+        if (expr.operator.type == TokenType.OR) {
+            if (isTruthy(left)) {
+                return left;
+            }
+        } else {
+            if (!isTruthy(left)) {
+                return left;
+            }
+        }
+
+        return evaluate(expr.right);
+    }
+
     private void checkNumberOperand(Token operator, Object operand) {
         if (operand instanceof Double) {
             return;
